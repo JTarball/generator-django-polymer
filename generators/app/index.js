da4098368129420ddca22464432ddfb89d0f6568
James Tarball <james.tarball@gmail.com>
2015-07-26 14:33:23 +0100

    Revert "updated package.son"
    This reverts commit 07cf4a5f94cec55910f2095e04540f34bbf86e34.

--- generators/app/index.js
+++ generators/app/index.js
@@ -13,7 +13,7 @@ var DjangoPolymerGenerator = generators.Base.extend({
         this.log('Firing generator polymer...');
         this.composeWith('polymer', {
             options: {
-                "skip-install": false
+                "skip-install": true
             }
         });
     },
@@ -70,19 +70,17 @@ var DjangoPolymerGenerator = generators.Base.extend({
             type: 'list',
             name: 'databaseEngine',
             message: 'Which database would you like to use? (mysql only available at present)',
-            choices: [
-                //{
-                //    value: 'mysql',
-                //    name: 'MySQL',
-                //}
+            choices: [{
+                    value: 'mysql',
+                    name: 'MySQL',
+                }
                 //, {
                 //    value: 'sqlite3',
                 //    name: 'SQLite3'
-                //}, 
-                {
-                   value: 'postgresql',
-                   name: 'PostgreSQL'
-                }
+                //}, {
+                //    value: 'postgresql',
+                //    name: 'PostgreSQL'
+                //}
             ]
         }, {
             type: 'input',
